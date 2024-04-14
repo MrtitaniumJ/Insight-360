f04556d43c2ec41c56ff84f8904e41a796f413b5
MrtitaniumJ <jkjatinsharma72@gmail.com>
2024-04-14 14:24:02 +0530

    authentication complete

--- backend/models/master/MasterInfo.js
+++ backend/models/master/MasterInfo.js
@@ -14,21 +14,21 @@ const masterInfoSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        minlength: 4, // Minimum length of 4 characters
-        maxlength: 20, // Maximum length of 20 characters
-        match: /^[a-zA-Z0-9_]+$/, // Only alphanumeric characters and underscores allowed
+        // minlength: 4, // Minimum length of 4 characters
+        // maxlength: 20, // Maximum length of 20 characters
+        // match: /^[a-zA-Z0-9_]+$/, // Only alphanumeric characters and underscores allowed
         trim: true // Trim whitespace from the beginning and end
     },
     email: {
         type: String,
         required: true,
         unique: true,
-        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ // Email validation regex
+        // match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ // Email validation regex
     },
     password: {
         type: String,
         required: true,
-        minlength: 6 // Minimum password length of 6 characters
+        // minlength: 6 // Minimum password length of 6 characters
     },
     profileImage: {
         type: String // Assuming you'll store the URL of the profile image
---
+++ backend/routes/masterRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const masterController = require('../controllers/master/masterController');
+const { authenticateUser } = require('../middleware/authentication');
+
+// User authentication routes
+router.post('/registerMaster', masterController.registerMaster);
+router.post('/loginMaster', masterController.loginMaster);
+router.post('/logoutMaster', masterController.logoutMaster);
+
+// Protected routes for users
+router.get('/master', authenticateUser, masterController.getMasterDetails);
+router.put('/master', authenticateUser, masterController.updateMasterDetails);
+router.delete('/master', authenticateUser, masterController.deleteMasterAccount);
+
+module.exports = router;
